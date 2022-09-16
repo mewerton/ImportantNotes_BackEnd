3dff446484db8b68c83fd75f0d4648d76dd54d0d
Mewerton Melo <mewerton@gmail.com>
2022-09-16 15:18:52 -0300

    Add files via upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,12 @@
 const { Router } = require("express")
 
+const UsersController = require("../controllers/UsersController")
+
 const usersRoutes = Router()
 
-usersRoutes.post("/users",(request, response) => {
+const usersController = new UsersController()
 
-    const { name, email, password } = request.body
-
-    response.json({name, email, password})
-
-})
+usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes 
